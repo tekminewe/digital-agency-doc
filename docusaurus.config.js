67a4e429fef9898bf635f133d5a68efe886085e2
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-02-21 21:23:41 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
 
   // Set the production url of your site here
   // url: 'https://tekminewe.github.io',
-  url: 'https://project.tekminewe.com',
+  url: 'https://digital-agency-website-template.docs.tekminewe.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/digital-agency-website-template/',
