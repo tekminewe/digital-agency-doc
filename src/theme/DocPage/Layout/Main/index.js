3cffe0565cffa7e0c348f871ff36332c8ecb2166
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-02-25 18:00:18 +0000

    Remove ads

--- src/theme/DocPage/Layout/Main/index.js
+++ src/theme/DocPage/Layout/Main/index.js
@@ -4,22 +4,6 @@ import Main from "@theme-original/DocPage/Layout/Main";
 export default function MainWrapper(props) {
   return (
     <>
-      <div style={{ marginBottom: "1rem" }}>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4412421169773097"
-          crossorigin="anonymous"
-        ></script>
-        <ins
-          class="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-4412421169773097"
-          data-ad-slot="5064107887"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      </div>
       <Main {...props} />
     </>
   );
--- src/theme/DocPage/Layout/Sidebar/index.js
+++ src/theme/DocPage/Layout/Sidebar/index.js
@@ -4,22 +4,6 @@ import Sidebar from "@theme-original/DocPage/Layout/Sidebar";
 export default function SidebarWrapper(props) {
   return (
     <>
-      <div style={{ marginBottom: "1rem" }}>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4412421169773097"
-          crossorigin="anonymous"
-        ></script>
-        <ins
-          class="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-4412421169773097"
-          data-ad-slot="5064107887"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      </div>
       <Sidebar {...props} />
     </>
   );
