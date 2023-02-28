fa4086b57966c2a092dfc74cf8aa9e448b48fbd8
Ewe Tek Min <tekminewe@users.noreply.github.com>
2023-02-28 08:06:18 +0000

    Add sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,12 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+        },
       }),
     ],
   ],
